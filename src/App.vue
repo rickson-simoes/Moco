b9f503d0d36af9aaf7076ca44281b991bbab12b9
Rickson <rickson.simoes@hotmail.com>
2020-11-28 12:33:46 -0300

    :wrench: Spaces

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,27 @@
 <!-- Container para inserir informações -->
     <b-container fluid='md'>
       <b-form @submit.prevent="onSubmit">
-          <b-form-input
-            class='mb-2'
-            v-model="form.productName"
-            type="text"
-            required
-            placeholder="Insira o nome do que foi adquirido."></b-form-input>
+        <b-form-input
+          class='mb-2'
+          v-model="form.productName"
+          type="text"
+          required
+          placeholder="Insira o nome do que foi adquirido."></b-form-input>
 
-          <b-form-input
-            class='mb-2'
-            v-model="form.value"
-            type="number"
-            step="0.01"
-            min="0.00"
-            required
-            placeholder="valor"></b-form-input>
+        <b-form-input
+          class='mb-2'
+          v-model="form.value"
+          type="number"
+          step="0.01"
+          min="0.00"
+          required
+          placeholder="valor"></b-form-input>
 
-          <b-form-input
-            class='mb-2'
-            v-model="form.productParcels"
-            type="number"
-            placeholder="valor das parcelas"></b-form-input>
+        <b-form-input
+          class='mb-2'
+          v-model="form.productParcels"
+          type="number"
+          placeholder="valor das parcelas"></b-form-input>
 
         <b-button type="submit" variant="primary">Submit</b-button>
         <b-button class='ml-4' type="reset" variant="danger" @click="clearFields()">Reset</b-button>
