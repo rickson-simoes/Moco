5c040efa5759798d9a9270fb672a990f815a7984
Rickson <rickson.simoes@hotmail.com>
2020-12-29 11:27:55 -0300

    :art: Only bootstrap / :wrench: Default 0 - parcels

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,85 @@
 <template>
   <div id="app">
-    <div class='flex-class'>
+  <b-navbar toggleable="lg" type="dark" variant="success" class='mb-4'>
+    <b-navbar-brand href="#">
+      <img src='./assets/money-trees.png' class="d-inline-block" width="35px"/>
+      MOney COntrol
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item active>MoCo</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+
+    <b-container fluid="lg">
       <!-- Container para exibir informações do app -->
-      <b-row>
-        <b-col class='ml-3'>
-          <img src='./assets/money-trees.png' width="400px"/>
+      <b-row class="text-center">
+        <!-- <b-col cols='1'>
+          <img src='./assets/money-trees.png' width='400px'/>
+        </b-col> -->
+
+        <!-- Container para inserir informações -->
+        <b-col>
+          <b-card border-variant="success">
+            <b-form @submit.prevent="onSubmit">
+              <b-form-input
+                class='mb-2'
+                v-model="form.productName"
+                type="text"
+                required
+                placeholder="Description"></b-form-input>
+
+              <b-form-input
+                class='mb-2'
+                v-model="form.value"
+                type="number"
+                step="0.01"
+                min="0.00"
+                required
+                placeholder="Value"></b-form-input>
+
+              <b-form-input
+                class='mb-2'
+                v-model="form.productParcels"
+                type="number"
+                placeholder="Parcels"></b-form-input>
+
+              <b-button type="submit" variant="success" >Submit</b-button>
+              <b-button class='ml-4' type="reset" variant="danger" @click="clearFields()">Reset</b-button>
+              <p/>
+              <h5><b-badge>Total : {{total}}</b-badge></h5>
+            </b-form>
+          </b-card>
         </b-col>
-
-      <!-- Container para inserir informações -->
-        <b-col class='listagem-items'>
-          <b-form @submit.prevent="onSubmit">
-            <b-form-input
-              class='mb-2'
-              v-model="form.productName"
-              type="text"
-              required
-              placeholder="Description"></b-form-input>
-
-            <b-form-input
-              class='mb-2'
-              v-model="form.value"
-              type="number"
-              step="0.01"
-              min="0.00"
-              required
-              placeholder="Value"></b-form-input>
-
-            <b-form-input
-              class='mb-2'
-              v-model="form.productParcels"
-              type="number"
-              placeholder="Parcels"></b-form-input>
-
-            <b-button type="submit" variant="success" >Submit</b-button>
-            <b-button class='ml-4' type="reset" variant="danger" @click="clearFields()">Reset</b-button>
-            <p/>
-            <h5><b-badge>Total : {{total}}</b-badge></h5>
-          </b-form>
 
           <!-- Container para exibir a lista de itens inseridos -->
-          <div class='mt-3'>
-            <template v-if='list.length === 0'>
-              <b-list-group class='mb-2' horizontal>
-                <b-list-group-item class='text-center flex-fill bg-light pt-5 pb-5'>Sem itens adicionados</b-list-group-item>
-              </b-list-group>
-            </template>
+        <b-col cols="7">
+          <b-card border-variant="success">
+            <div class='mt-3'>
+              <template v-if='list.length === 0'>
+                <b-list-group class='mb-2' horizontal>
+                  <b-list-group-item class='text-center flex-fill bg-light pt-5 pb-5'>Sem itens adicionados</b-list-group-item>
+                </b-list-group>
+              </template>
 
-            <template v-else>
-              <b-list-group class='mb-2' v-for="(items, index) in list" :key='index' horizontal>
-                <b-list-group-item @click="Delete(index)"> <b-icon icon='trash' title="Delete"/> </b-list-group-item>
-                <b-list-group-item @click="Edit()"> <b-icon icon='pencil' title="Edit"/> </b-list-group-item>
-                <b-list-group-item style='width: 300px;' class='flex-fill bg-light'>{{items.productName}}</b-list-group-item>
-                <b-list-group-item style='width: 50px; padding:5px 5px;' class='flex-fill bg-light'>{{items.value}}</b-list-group-item>
-                <b-list-group-item style='width: 20px;' class='flex-fill bg-light'>{{items.productParcels}}</b-list-group-item>
-              </b-list-group>
-            </template>
-          </div>
+              <template v-else>
+                <b-list-group class='mb-2' v-for="(items, index) in list" :key='index' horizontal>
+                  <b-list-group-item @click="Delete(index)"> <b-icon icon='trash' title="Delete"/> </b-list-group-item>
+                  <b-list-group-item @click="Edit()"> <b-icon icon='pencil' title="Edit"/> </b-list-group-item>
+                  <b-list-group-item class='w-100 bg-light'>{{items.productName}}</b-list-group-item>
+                  <b-list-group-item class='w-25 bg-light'>{{items.value}}</b-list-group-item>
+                  <b-list-group-item class='text-center bg-light'>{{items.productParcels}}</b-list-group-item>
+                </b-list-group>
+              </template>
+            </div>
+          </b-card>
         </b-col>
       </b-row>
-    </div>
+    </b-container>
   </div>
 </template>
 
@@ -70,7 +91,7 @@ export default {
       form: {
         productName: '',
         value: '',
-        productParcels: '',
+        productParcels: 0,
       },
       list: [],
       total: 0,
@@ -135,16 +156,5 @@ export default {
 // cOLOR #1f1a17;
 // cOLOR #00763a;
 // cOLOR #00a13a;
-
-.flex-class {
-  display:flex;
-}
-
-.listagem-items {
-    border: 1px solid #c2e094;
-    border-radius: 10px;
-    padding: 10px;
-    background: #98a597;
-}
 
 </style>
